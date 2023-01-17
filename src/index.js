b89e88a999b82aa8f5482b3861ce64192d3e78c8
vlad-khrychov <67462207+vlad-khrychov@users.noreply.github.com>
2023-01-17 17:55:54 +0200

    splitting into components

--- src/index.js
+++ src/index.js
@@ -1,25 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+
+const Score = (props) => {
+  const [score, setScore] = useState(10);
+  return (
+    <div className="score-container">
+      <h1>Your Score</h1>
+      <div className="score-wrap">
+        <div className="score">{score}</div>
+        <button
+          className="score-button"
+          onClick={() => {
+            setScore(0);
+          }}
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const ButtonAdd = (props) => {
+  const { number } = props;
+  return <button className="score-button">+{number}</button>;
+};
+
+const AddingScore = (props) => {
+  return (
+    <div className="add-score-container">
+      <h1>Add points</h1>
+      <div className="score-buttons">
+        <ButtonAdd number={1} />
+        <ButtonAdd number={5} />
+        <ButtonAdd number={10} />
+      </div>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <div className="App">
-      <div className="score-container">
-        <h1>Your Score</h1>
-        <div className="score-wrap">
-          <div className="score">10</div>
-          <button className="score-button">Reset</button>
-        </div>
-      </div>
-      <div className="add-score-container">
-        <h1>Add points</h1>
-        <div className="score-buttons">
-          <button className="score-button">+1</button>
-          <button className="score-button">+5</button>
-          <button className="score-button">+10</button>
-        </div>
-      </div>
+      <Score />
+      <AddingScore />
     </div>
   );
 };
