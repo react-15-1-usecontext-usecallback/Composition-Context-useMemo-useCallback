d6ed8428414e0be4e740541bc82b28dbb1a3fcc3
vlad-khrychov <67462207+vlad-khrychov@users.noreply.github.com>
2023-01-17 18:13:42 +0200

    realization via props

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 const Score = (props) => {
-  const [score, setScore] = useState(10);
+  const { score, setScore } = props;
   return (
     <div className="score-container">
       <h1>Your Score</h1>
@@ -23,28 +23,42 @@ const Score = (props) => {
 };
 
 const ButtonAdd = (props) => {
-  const { number } = props;
-  return <button className="score-button">+{number}</button>;
+  const { number, addScore } = props;
+  return (
+    <button
+      className="score-button"
+      onClick={() => {
+        addScore(number);
+      }}
+    >
+      +{number}
+    </button>
+  );
 };
 
 const AddingScore = (props) => {
+  const { addScore } = props;
   return (
     <div className="add-score-container">
       <h1>Add points</h1>
       <div className="score-buttons">
-        <ButtonAdd number={1} />
-        <ButtonAdd number={5} />
-        <ButtonAdd number={10} />
+        <ButtonAdd number={1} addScore={addScore} />
+        <ButtonAdd number={5} addScore={addScore} />
+        <ButtonAdd number={10} addScore={addScore} />
       </div>
     </div>
   );
 };
 
 const App = () => {
+  const [score, setScore] = useState(10);
+  const addScore = (number) => {
+    setScore(score + number);
+  };
   return (
     <div className="App">
-      <Score />
-      <AddingScore />
+      <Score score={score} setScore={setScore} />
+      <AddingScore addScore={addScore} />
     </div>
   );
 };
